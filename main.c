53b080082e934549f1241ba19d8e11437ebfaade
Smirow <magued1509@gmail.com>
2017-04-04 09:33:24 +0200

    fixes

--- main.c
+++ main.c
@@ -245,14 +245,21 @@ void print_matrix(Matrix_filter matrix, int rows, int cols) {
 }
 
 int main(int argc, char **argv) {
+    if (argc != 3)
+        abort_("usage: ./a.out png_source_file png_dest_filename");
+
     FILE *fp = fopen(argv[1], "rb");
     if (!fp)
         abort_("[read_png_file] File %s could not be opened for reading", argv[1]);
+    
     Master_png master_png = init_master_png(fp);
-    MatrixPNG matrix = initPngMatrix(master_png.main, master_png.info);   
+    MatrixPNG matrix = initPngMatrix(master_png.main, master_png.info);
     printf("W: %d, H: %d\n", matrix->cols, matrix->rows);
+
     MatrixPNG matrix_copy = gauss_blur(matrix, master_png, 4, 7);
-    write_png_file("out.png", matrix_copy);
+    write_png_file(argv[2],  matrix_copy);
+
     fclose(fp);
+
     return 0;
 }
